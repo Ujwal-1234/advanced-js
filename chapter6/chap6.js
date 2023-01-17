2f9af2b63db4b552f5d2d8e10909d4d7825971b7
Ujwal Kumar Mahato <kujwal147@gmail.com>
2023-01-17 17:30:11 +0530

    chapter 6 inheritence and chaining

---
+++ chapter6/chap6.js
@@ -0,0 +1,30 @@
+// prototype chaining
+
+function Player(playername, goal){
+    this.playername = playername
+    this.score = goal
+}
+Player.prototype._goal = function(goal){
+    this.score+=goal
+}
+
+const player1 = new Player("dhoni", 5)
+
+Player.prototype.penality = function(){
+    console.log("Loosing 1 score")
+    this.score-=1
+}
+
+player1.penality()  
+
+console.log(player1.score)
+
+
+// inheritence in the protype chain
+function _champion(playername, goal){
+    Player.call(this, playername)       // Here the Player method is called with the context of arguments of _champion
+    this.score = goal
+}
+_champion.prototype = Object.create(Player.prototype)   
+
+const player2 = new _champion("ronaldo", 20)
