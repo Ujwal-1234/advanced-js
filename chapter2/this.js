ae1cedca481b4ced1b5b83385fb84c55657f1380
Ujwal Kumar Mahato <kujwal147@gmail.com>
2023-01-17 11:30:43 +0530

    binding complete

--- chapter2/this.js
+++ chapter2/this.js
@@ -54,3 +54,67 @@ const me = new new_User("Tyler", 27);
 const you = new new_User("stark", 40);
 alert(`hey, ${me.name}, your age is ${me.age}`);
 alert(`hey, ${you.name}, your age is ${you.age}`);
+// new bindings enables to create a kind of prototype that can be used multiple times 
+// by creating new objects using that function()/prototype
+
+
+
+
+
+// lexical binding
+
+const lexical_user = {
+    name: "Stark",
+    age: 42,
+    languages: ["JS", "C++", "Python", "JAVA"],
+    greet() {
+        const hello = `Hello, my name is ${this.name} and I know`;
+    
+        const langs = this.languages.reduce(function (str, lang, i) {
+            if (i === this.languages.length - 1) {
+            return `${str} and ${lang}.`;
+            }
+            return `${str} ${lang},`;
+        }.bind(this), "");
+        console.log(lexical_user.languages) // to check if the array values are changed after the reduce() method called
+        alert(hello + langs);
+  },
+};
+lexical_user.greet()
+console.log(lexical_user.languages) // to check if the array values are changed after the reduce() method called
+
+// .reduce() is the method, used to perform operation on the array based on the parameters provided.
+// here in above the reduce() method is used for string concatination for the string data present in array languages.
+// the reduce() method doesnot affect or change the array after operation performed.
+
+
+
+
+// window binding
+
+window.initial = "Hey Good";            // The variables are declared and binded with windows and can be accessed in any functions using this refrence
+
+function _person_greet(time){
+    switch(time){
+        case "morning": alert(`${this.initial} ${this.morning_greet} ... ${user_name}`);break;
+        case "afternoon" : alert(`${this.initial} ${this.afternoon} ... ${user_name}`);break;
+        default : alert(`Hey Welcome .. ${user_name}`);break;
+    }
+}
+const user_name = prompt("Tell me your name")       // To take in the user_name value
+function morning(){
+    window.morning_greet = " Morning";
+}
+function afternoon(){
+    window.afternoon =" Afternoon";
+}
+morning()
+_person_greet("morning")
+afternoon()
+_person_greet("afternoon")
+_person_greet("esajadsf");
+
+// above is about binding the variables with the window global object
+// can be accessed in any function one declared by binding with window global object
+
+// NOTE ::-- "use strict"; is a statement if written in the document, the this refrence will become undefined and the window binding doesnot work.
